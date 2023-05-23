d953e98cb57ef1829325f22ba8d7117fb47abb42
SoftGitter <cobynine2@gmail.com>
2023-05-23 14:29:38 +0900

    2023.05.23 처음 architecture 파일

---
+++ src/main/java/com/hun/edu/App.java
@@ -0,0 +1,13 @@
+package com.hun.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
